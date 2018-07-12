2d397c178e0f8dee6bbd22d1d63184ed85509605
vishwassathish <vishwassathish@gmail.com>
2018-07-13 04:03:11 +0530

    Add files via upload

---
+++ interpretable_models/interpretable_classifiers.py
@@ -0,0 +1,21 @@
+# Robust explainable framework for interpretation of machine learning models #
+
+from sklearn import linear_model
+import pandas as pd
+
+
+class Expl_Logistic_Regression():
+# Class to wrap sklearn's logistic regression, few of LIME's interpretable modules and other custom functions for explaining prediction.
+# Using 'Expl' prefix for wrapper classes used for explainable framework 
+
+	def __init__(self, X, y, split_ratio=0.66):
+	# X : numpy array or python list of shape (n_samples, n_attributes)
+	# y : array of corresponding target class.
+	# split_ratio : Ratio representing how much of X is to be considered as  training samples.
+
+	self.x_train = 
+
+
+
+if __name__ == '__main__' :
+
